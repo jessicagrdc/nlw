ef4503ac393453c79db3ffb79639be281c5311fc
jessicagrdc <jessicagrdc@gmail.com>
2020-07-06 14:08:58 -0300

    commit on feature/jessicagrdc

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const server = express()
 
+// pegar o banco de dados
+const db = require("./database/db")
+
 server.use(express.static("public"))
 
 const nunjucks = require("nunjucks")
@@ -17,7 +20,14 @@ server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
 })
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+    // pegar os dados do banco de dados
+    db.all(`SELECT name FROM places`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+        return res.render("search-results.html", { places: rows})
+     })
+    
 })
 
 server.listen(3000)
