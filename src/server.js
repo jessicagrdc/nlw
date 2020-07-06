916472b2ea8eecdf325921165fa1daa6d345c0db
jessicagrdc <jessicagrdc@gmail.com>
2020-07-06 16:35:12 -0300

    commit on feature/jessicagrdc

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const db = require("./database/db")
 
 server.use(express.static("public"))
 
+server.use(express.urlencoded({ extended: true }))
+
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
     express: server,
@@ -13,21 +15,67 @@ nunjucks.configure("src/views", {
 })
 
 server.get("/", (req, res) => {
-    return res.render("index.html", {title: "Um título"})
+    return res.render("index.html", { title: "Um título" })
 })
 
+
 server.get("/create-point", (req, res) => {
+    console.log(req.query)
+
     return res.render("create-point.html")
 })
+
+server.post("/savepoint", (req, res) => {
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+    if (err) {
+        return console.log(err)
+    }
+
+    console.log("Cadastrado com sucesso")
+    console.log(this)
+
+    return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+
+    
+})
+
+
 server.get("/search", (req, res) => {
     // pegar os dados do banco de dados
-    db.all(`SELECT name FROM places`, function (err, rows) {
+
+    db.all(`SELECT * FROM places`, function (err, rows) {
         if (err) {
             return console.log(err)
         }
-        return res.render("search-results.html", { places: rows})
-     })
-    
+        const total = rows.length
+        return res.render("search-results.html", { places: rows, total })
+    })
+
 })
 
 server.listen(3000)
